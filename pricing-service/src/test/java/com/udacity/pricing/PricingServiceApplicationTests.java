0ab4f7f8cb376f9a5e1af738b3eb5b4955a7122a
luke donley <titustwimbly@lukes-MacBook-Pro.local>
2020-11-02 10:31:21 -0600

    changes to vehicles api, configuration and fixes

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,38 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.api.PricingController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(controllers = {PricingController.class})
 public class PricingServiceApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void getPrice() throws Exception
+	{
+		mvc.perform(MockMvcRequestBuilders.get("/services/price?vehicleId=6")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.currency").isString())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.price").isNumber())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.vehicleId").value(6));
 	}
 
 }
